6b3f02642acfd6f9c846b7ababffffdb7b8e7c3b
@MrDenli <89518020310@mail.ru>
2023-12-19 17:25:47 +0300

    registration ready

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,7 +1,52 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
 
 app = Flask(__name__)
+def connect_to_db():
+    try:
+        connection = psycopg2.connect(
+            host = 'localhost',
+            database = 'bd',
+            user = 'admin',
+            password = 'root',
+            port = '5433'
+        )
+        print("Успешное подключение к базе данных")
+        return connection
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Ошибка при подключении к базе данных", error)
+        return None
+
+#Маршрут для отображения формы регистрации
+@app.route('/registration', methods=['GET'])
+def show_registration_form():
+    return render_template("registration.html")
+
+# Маршрут для обработки данных из формы регистрации
+@app.route('/registration', methods=['POST'])
+def process_registration_form():
+    # Получаем данные из формы
+    name = request.form.get('username')
+    surname = request.form.get('surname')
+    sex = request.form.get('sex')
+    passport_number = request.form.get('passport_number')
+    password = request.form.get('password')
+    # Подключаемся к базе данных
+    connection = connect_to_db()
+    if connection:
+        try:
+            # Используем контекстный менеджер для управления курсором
+            with connection.cursor() as cursor:
+                # Ваш SQL-запрос для вставки данных в базу данных
+                sql_query = "INSERT INTO person (name, surname, sex, password, passport_number) VALUES (%s, %s, %s, %s, %s);"
+                cursor.execute(sql_query, (name, surname, sex, password, passport_number))
+                connection.commit()
+            print("Данные успешно записаны в базу данных")
+        except Exception as error:
+            print("Ошибка при записи данных в базу данных", error)
+        finally:
+            connection.close()
+    return redirect(url_for('show_registration_form'))
 
 @app.route('/')
 @app.route('/home')
